39ab527e295eb3d9262d5414cb65295c1a85dae4
Vale Quiroga <valentinqramos@outlook.com>
2022-08-28 15:05:30 -0500

    Adding new validations

--- controllers/community.controller.js
+++ controllers/community.controller.js
@@ -7,12 +7,12 @@ exports.allThePosts = async (req, res) => {
   try {
     const posts = await Community.find();
 
-    if (!posts) 
-    return  res.status(400).json({ errorMessage: "Posts not found" });
+    if (!posts)
+      return res.status(400).json({ errorMessage: "Posts not found" });
 
     res.status(200).json({ posts });
   } catch (error) {
-    if (error instanceof mongoose.Error.ValidationError) 
+    if (error instanceof mongoose.Error.ValidationError)
       return res.status(400).json({ errorMessage: error.message });
     return res.status(500).json({ errorMessage: error.message });
   }
@@ -23,8 +23,8 @@ exports.posting = async (req, res) => {
   const { author, comment, created } = req.body;
   const { username } = req.user;
   try {
-    if (!comment.length) 
-    return res.status(400).json({
+    if (!comment.length)
+      return res.status(400).json({
         errorMessage: "don't send empty fields",
       });
 
@@ -37,7 +37,7 @@ exports.posting = async (req, res) => {
     const cleanPost = clearRes(post.toObject());
     res.status(200).json({ post: cleanPost });
   } catch (error) {
-    if (error instanceof mongoose.Error.ValidationError) 
+    if (error instanceof mongoose.Error.ValidationError)
       return res.status(400).json({ errorMessage: error.message });
     return res.status(500).json({ errorMessage: error.message });
   }
@@ -47,14 +47,20 @@ exports.posting = async (req, res) => {
 exports.updatePost = async (req, res) => {
   const { comment } = req.body;
   const { id } = req.params;
+  const { username , role } = req.user
+
   try {
     const post = await Community.findById(id);
 
-    if (!post) 
+    if (!post)
       return res.status(400).json({
         errorMessage: "Community post not found.",
       });
     
+    console.log(post.author , username , role)
+    if (username != post.author) return res.status(400).json({
+      errorMessage: "Only the original author can delete this post"
+    })
 
     const updatePost = await Community.findByIdAndUpdate(
       id,
@@ -65,8 +71,8 @@ exports.updatePost = async (req, res) => {
     const cleanPost = clearRes(updatePost.toObject());
     res.status(200).json({ updatePost: cleanPost });
   } catch (error) {
-    if (error instanceof mongoose.Error.ValidationError) 
-      return res.status(400).json({ errorMessage: error.message });   
+    if (error instanceof mongoose.Error.ValidationError)
+      return res.status(400).json({ errorMessage: error.message });
     return res.status(500).json({ errorMessage: error.message });
   }
 };
@@ -74,14 +80,18 @@ exports.updatePost = async (req, res) => {
 // DELETE community post
 exports.deletePost = async (req, res) => {
   const { id } = req.params;
+  const { username , role } = req.user;
 
   try {
     const post = await Community.findById(id);
-    if (!post)
-    return  res.status(400).json({ errorMessage: "Post not found" });
+    if (!post) return res.status(400).json({ errorMessage: "Post not found" });
+
+    if (username != post.author && role === 'User' ) return res.status(400).json({ 
+      errorMessage: "Only the original author or an administrator can delete this post" 
+    })
 
     await post.remove();
-    res.status(200).json({ successMessage: "Post deleted" });
+    res.status(200).json({ successMessage: `Hi ${username}, you delete youre post` });
   } catch (error) {
     if (error instanceof mongoose.Error.ValidationError)
       return res.status(400).json({ errorMessage: error.message });
